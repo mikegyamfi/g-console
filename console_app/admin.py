dca17f9c4ce01b40cad78a49f5e95567bca52839
mikegyamfi <codeage20@gmail.com>
2024-02-20 08:00:26 +0000

    initial commit

--- console_app/admin.py
+++ console_app/admin.py
@@ -2,8 +2,16 @@ from django.contrib import admin
 
 from console_app import models
 
+
+class NewTransactionAdmin(admin.ModelAdmin):
+    list_per_page = 800
+    list_display = ["user", "receiver", "reference", "bundle_amount", "transaction_date",
+                    "transaction_status"]
+    search_fields = ["receiver", 'reference', "bundle_amount"]
+
+
 # Register your models here.
 admin.site.register(models.CustomUser)
 admin.site.register(models.UserProfile)
-admin.site.register(models.NewTransaction)
+admin.site.register(models.NewTransaction, NewTransactionAdmin)
 admin.site.register(models.CreditingHistory)
