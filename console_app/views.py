f6ded51ff6d0d3e16ef0f762af21dd14edbc2b82
mikegyamfi <codeage20@gmail.com>
2024-08-29 13:10:38 +0000

    hi

--- console_app/views.py
+++ console_app/views.py
@@ -221,7 +221,8 @@ def register(request):
             user.save()
             if models.UserProfile.objects.filter(user=user).exists():
                 return redirect('login')
-            user_profile_data.save()
+            else:
+                user_profile_data.save()
             messages.success(request, "Sign Up Successful. Your account creation has been submitted for approval.")
             return redirect('login')
     context = {'form': form}
